0df7070d243b1bd5510d1973b68b7aa51894973c
Aleena <231865945+aleena1298@users.noreply.github.com>
2026-09-07 11:09:14 +0500

    Slider fixed

--- src/components/MarketplaceSlider/MarketplaceSlider.jsx
+++ src/components/MarketplaceSlider/MarketplaceSlider.jsx
@@ -1,6 +1,10 @@
-import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Scrollbar } from "swiper/modules";
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+} from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/navigation";
@@ -16,13 +20,24 @@ import mio from "../../assets/images/Yamaha mio 3.png";
 import gearUltimata from "../../assets/images/Yamaha gear ultimata 3.png";
 
 export default function MarketplaceSlider() {
-  const sliderData = [aerox, nmaxStd, nmaxTech, mio, gearUltimata];
+  const sliderData = [
+    aerox,
+    nmaxStd,
+    nmaxTech,
+    mio,
+    gearUltimata,
+  ];
 
   return (
     <div className="marketplace-slider">
       <div className="marketplace-slider-inside">
         <Swiper
-          modules={[Navigation, Pagination, Scrollbar]}
+          modules={[
+            Navigation,
+            Pagination,
+            Scrollbar,
+            A11y,
+          ]}
           spaceBetween={10}
           slidesPerView={1}
           navigation={true}
@@ -32,14 +47,15 @@ export default function MarketplaceSlider() {
           scrollbar={{
             draggable: true,
           }}
-          onSwiper={(swiper) => console.log(swiper)}
-          onSlideChange={() => console.log("slide change")}
         >
           {sliderData.map((image, index) => (
             <SwiperSlide key={index}>
               <div className="marketplace-slide">
                 <div className="marketplace-slide-inside">
-                  <img src={image} alt="Yamaha bike" />
+                  <img
+                    src={image}
+                    alt="Yamaha bike"
+                  />
 
                   <h1></h1>
                 </div>
